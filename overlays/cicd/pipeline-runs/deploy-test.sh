d8737f026516eded59aa8dbf2924852a432cd3c4
Andrew Pitt <apitt@redhat.com>
2023-10-25 08:32:40 -0400

    Update pipeline runs

--- overlays/cicd/pipeline-runs/deploy-test.sh
+++ overlays/cicd/pipeline-runs/deploy-test.sh
@@ -2,6 +2,9 @@
 
 SKOPEO_TAGS=$(skopeo list-tags docker://quay.io/pittar/petclinic-demo | jq -c .Tags | tail -c +2 | head -c -2 | tr -d \")
 
+GIT_REPO="http://gitea.scm.svc.cluster.local:3000/developer/petclinic-gitops.git"
+IMAGE_URL="quay.io/pittar/petclinic-demo"
+
 set -f
 TAGS=(${SKOPEO_TAGS//,/ })
 
@@ -20,13 +23,16 @@ select tag in "${TAGS[@]}"; do
 
     echo "Preparing to deploy $tag"
 
-    tkn pipeline start deploy-to-test \
-        --param image_tag="$tag" \
-        --workspace name=gitops,claimName=gitops \
-        -n petclinic-cicd
+    command="tkn pipeline start deploy-to-test --param image_tag=\"$tag\" --use-param-defaults=true --workspace name=gitops,claimName=gitops -n petclinic-cicd | tail -1"
 
-    tkn pipeline logs -f deploy-to-test \
-        -n petclinic-cicd
+    echo "Command: $command"
+
+    $(eval $command)
+    
+    #echo "Output: $output"
+
+    #tkn pipeline logs -f deploy-to-test \
+    #    -n petclinic-cicd
 
     exit 0
 done
